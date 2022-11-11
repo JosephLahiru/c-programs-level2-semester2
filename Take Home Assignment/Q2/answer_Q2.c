8544a1602ded77d970af4b1b91287c13d4ba8fdc
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-11-11 18:26:42 +0000

    created asnswer_Q2

---
+++ Take Home Assignment/Q2/answer_Q2.c
@@ -0,0 +1,7 @@
+//A call to wait() blocks the calling process until one of its 
+//child processes exits or a signal is received. After child process terminates, 
+//parent continues its execution after wait system call instruction.
+
+//The C library function void (*signal(int sig, void (*func)(int)))(int) sets 
+//a function to handle signal i.e. a signal handler with signal number sig.
+
