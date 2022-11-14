bfe1c74f089a575de4ac9acf3989396c4156d3d2
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-11-14 07:15:11 +0000

    Finished Q2

--- Take Home Assignment/Q2/answer_Q2.c
+++ Take Home Assignment/Q2/answer_Q2.c
@@ -1,7 +1,34 @@
-//A call to wait() blocks the calling process until one of its 
-//child processes exits or a signal is received. After child process terminates, 
-//parent continues its execution after wait system call instruction.
+#include<stdio.h>
 
-//The C library function void (*signal(int sig, void (*func)(int)))(int) sets 
-//a function to handle signal i.e. a signal handler with signal number sig.
+int main(){
+    while(true){
+        // Reader wants to enter the critical section
+        wait(mutex);
 
+        // The number of readers has now increased by 1
+        read_count++;                          
+
+        // there is atleast one reader in the critical section
+        // this ensure no writer can enter if there is even one reader
+        // thus we give preference to readers here
+        if (read_count==1)     
+            wait(rw_mutex);                    
+
+        // other readers can enter while this current reader is inside 
+        // the critical section
+        signal(mutex);                   
+
+        // current reader performs reading here
+        wait(mutex);   // a reader wants to leave
+
+        read_count--;
+
+        // that is, no reader is left in the critical section,
+        if (read_count == 0) 
+            signal(rw_mutex);         // writers can enter
+
+        signal(mutex); // reader leaves
+    }
+
+    return 0;
+}
