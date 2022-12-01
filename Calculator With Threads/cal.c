ae8682625f544f4a82f93d461346a77c0756751b
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-12-01 13:14:30 +0000

    declared basic cal functions

--- Calculator With Threads/cal.c
+++ Calculator With Threads/cal.c
@@ -1,7 +1,28 @@
 #include<stdio.h>
 
+void add(float x, float y);
+void substract(float x, float y);
+void multiply(float x, float y);
+void devide(float x, float y);
+
 int main(){
     printf("Hello World!");
 
     return 0;
+}
+
+void add(float x, float y){
+
+}
+
+void subtract(float x, float y){
+
+}
+
+void multiply(float x, float y){
+
+}
+
+void devide(float x, float y){
+
 }
