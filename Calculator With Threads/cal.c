7f32fd87fbb3df5de0c368547d05a0f14b932a22
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-12-01 13:27:16 +0000

    Added do while loop and switch structure

--- Calculator With Threads/cal.c
+++ Calculator With Threads/cal.c
@@ -6,7 +6,39 @@ void multiply(float x, float y);
 void devide(float x, float y);
 
 int main(){
-    printf("Hello World!");
+
+    int op;
+
+    do{
+        printf("\n------- Basic Calculator -------\n\n");
+        printf("\t1. Add\n\t2. Substract\n\t3. Multiply\n\t4. Devide\n\t5. Exit\n\n");
+        printf("Please select an option : ");
+
+        scanf("%d", &op);
+
+        switch (op)
+        {
+            case 1:
+                //add
+                break;
+            case 2:
+                //Subtract
+                break;
+            case 3:
+                //multiply
+                break;
+            case 4:
+                //devide
+                break;
+            case 5:
+                exit(0);
+                break;
+            default:
+                printf("Please check your selected item!!");
+                break;
+        }
+
+    }while(op!=5);
 
     return 0;
 }
