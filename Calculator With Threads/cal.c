24ca05bbe1aa1626798d45b52ca4961667a10375
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-12-01 14:57:46 +0000

    finished code with fork

--- Calculator With Threads/cal.c
+++ Calculator With Threads/cal.c
@@ -1,15 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void add(float x, float y);
-void substract(float x, float y);
-void multiply(float x, float y);
-void devide(float x, float y);
+float add(float x, float y);
+float substract(float x, float y);
+float multiply(float x, float y);
+float devide(float x, float y);
 
 int main(){
 
     int op;
     float x, y;
+
+    pid_t pid1, pid2, pid3, pid4;
 
     do{
         printf("\n------- Basic Calculator -------\n\n");
@@ -25,49 +28,51 @@ int main(){
             scanf("%f", &y);
         }
 
-        switch (op)
-        {
-            case 1:
-                //add
-                add(x, y);
-                break;
-            case 2:
-                //Subtract
-                substract(x, y);
-                break;
-            case 3:
-                //multiply
-                multiply(x, y);
-                break;
-            case 4:
-                //devide
-                devide(x, y);
-                break;
-            case 5:
-                exit(0);
-                break;
-            default:
-                printf("Please check your selected item!!");
-                break;
+        pid1 = fork();
+        if(pid1 < 0){
+            printf("\ncreating child 1 error.\n");
+        }else if(pid1 == 0 && op == 1){
+            printf("\nThe value after adding is %.2f\n", add(x, y));
+        }else{
+            pid2 = fork();
+            if(pid2 < 0){
+                printf("\ncreating child 2 error.\n");
+            }else if(pid2 == 0 && op == 2){
+                printf("\nThe value after substracting is %.2f\n", substract(x, y));
+            }else{
+                pid3 = fork();
+                if(pid3 < 0){
+                    printf("\ncreating child 3 error.\n");
+                }else if(pid3 == 0  && op == 3){
+                    printf("\nThe value after multiplying is %.2f\n", multiply(x, y));
+                }else{
+                    pid4 = fork();
+                    if(pid4 < 0 ){
+                        printf("\ncreating child 4 error.\n");
+                    }else if(pid4 == 0  && op == 4){
+                        printf("\nThe value after substracting is %.2f\n", devide(x, y));
+                    }
+                }
+            }
         }
-
+        
     }while(op!=5);
 
     return 0;
 }
 
-void add(float x, float y){
-    printf("\nValue after adding %.2f\n", x+y);
+float add(float x, float y){
+    return (x+y);
 }
 
-void substract(float x, float y){
-    printf("\nValue after substracting %.2f\n", x-y);
+float substract(float x, float y){
+    return (x-y);
 }
 
-void multiply(float x, float y){
-    printf("\nValue after multiplying %.2f\n", x*y);
+float multiply(float x, float y){
+    return (x*y);
 }
 
-void devide(float x, float y){
-    printf("\nValue after deviding %.2f\n", x/y);
+float devide(float x, float y){
+    return (x/y);
 }
